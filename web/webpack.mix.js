8355ab164625516a1a2d0f1c20a8d45c380daa9f
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-06-17 20:28:33 +0900

    add_vue.jsを追加

--- web/webpack.mix.js
+++ web/webpack.mix.js
@@ -13,3 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync({
+    proxy: '0.0.0.0:8081',
+    open: false
+  })
+  .js('resources/js/app.js', 'public/js')
+  .version()
