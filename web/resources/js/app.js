221ce55984ae37005fd886a2fa914d43ec67e413
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-06-18 02:20:35 +0900

    add_ルートコンポーネント作成

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import router from './router.js'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App />'
+})
