f7d2f4737f86f3fa2b7ac5c9b8223f954dcda0f1
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-06-18 02:27:11 +0900

    add_ルーティング追加

---
+++ web/resources/js/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// ページコンポーネント
+import PhotoList from './pages/PhotoList.vue'
+import Login from './pages/Login.vue'
+
+// VueRouter
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: PhotoList
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
+
