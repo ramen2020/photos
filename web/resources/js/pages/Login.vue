bba05d4680a0c8a933386368369c2f627144147a
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-06-18 20:43:24 +0900

    fix_ログインコンポーネント修正

--- web/resources/js/pages/Login.vue
+++ web/resources/js/pages/Login.vue
@@ -1,3 +1,76 @@
 <template>
-  <h1>Login</h1>
+  <div class="container--small">
+    <ul class="tab">
+      <li
+        class="tab__item"
+        :class="{'tab__item--active': tab === 1 }"
+        @click="tab = 1"
+      >Login</li>
+      <li
+        class="tab__item"
+        :class="{'tab__item--active': tab === 2 }"
+        @click="tab = 2"
+      >Register</li>
+    </ul>
+    <!-- ログインフォーム -->
+    <div class="panel" v-show="tab === 1">
+      <form class="form" @submit.prevent="login">
+        <label for="login-email">Email</label>
+        <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
+        <label for="login-password">Password</label>
+        <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">login</button>
+        </div>
+      </form>
+    </div>
+    <!-- 新規登録フォーム -->
+    <div class="panel" v-show="tab === 2">
+      <form class="form" @submit.prevent="register">
+        <label for="username">Name</label>
+        <input type="text" class="form__item" id="username" v-model="registerForm.name">
+        <label for="email">Email</label>
+        <input type="text" class="form__item" id="email" v-model="registerForm.email">
+        <label for="password">Password</label>
+        <input type="password" class="form__item" id="password" v-model="registerForm.password">
+        <label for="password-confirmation">Password (confirm)</label>
+        <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">register</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: 1,
+      loginForm: {
+        email: '',
+        password: ''
+      },
+      registerForm: {
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+      }
+    }
+  },
+  methods: {
+    async register () {
+      // authストアのresigterアクションを呼び出す。await非同期なアクションを完了まってから始まる
+      await this.$store.dispatch('auth/register', this.registerForm)
+      // トップページに移動する
+      this.$router.push('/')
+    },
+    async login () {
+      await this.$store.dispatch('auth/login', this.loginForm)
+      this.$router.push('/')
+    }
+  }
+}
+</script>
