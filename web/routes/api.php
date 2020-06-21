0a9186c703e1a26868a071ac24dc420d01195e4d
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-06-22 01:42:12 +0900

    add_写真一覧機能のルーティングを追加

--- web/routes/api.php
+++ web/routes/api.php
@@ -25,3 +25,5 @@ Route::get('/user', fn() => Auth::user())->name('user');
 
 // 写真投稿
 Route::post('/photos', 'PhotoController@create')->name('photo.create');
+// 写真一覧
+Route::get('/photos', 'PhotoController@index')->name('photo.index');
