65b6173841c018b91ded768df9685bf4b9a687b6
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-06-22 00:26:50 +0900

    add_Photoモデル作成

---
+++ web/app/Photo.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+
+class Photo extends Model
+{
+    /** プライマリキーの型 */
+    protected $keyType = 'string';
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        if (! Arr::get($this->attributes, 'id')) {
+            $this->setId();
+        }
+    }
+
+    /**
+     * ランダムなID値をid属性に代入
+     *
+     */
+    private function setId()
+    {
+        $this->attributes['id'] = $this->getRandomId(12);
+    }
+
+    /**
+     * ランダムなID値を生成
+     *
+     * @return string
+     */
+    private function getRandomId($sum)
+    {
+        $characters = array_merge(
+            range(0, 9), range('a', 'z'),
+            range('A', 'Z'), ['-', '_']
+        );
+
+        $length = count($characters);
+        $id = "";
+
+        for ($i = 0; $i < $sum; $i++) {
+            $id .= $characters[random_int(0, $length - 1)];
+        }
+
+        return $id;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
