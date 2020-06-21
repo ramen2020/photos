c0948acb1daba70c6e24db50e1155691e8a45edd
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-06-22 01:41:51 +0900

    add_写真表示コンポーネント追加

---
+++ web/resources/js/components/Photo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="photo">
+    <figure class="photo__wrapper">
+      <img
+        class="photo__image"
+        :src="'/img/photo_images/'+item.filename"
+        :alt="`Photo by ${item.user.name}`"
+      >
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
