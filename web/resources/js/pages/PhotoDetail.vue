6329c93998d19d3c16b18a606bae2df1b10518bb
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-06-24 23:50:46 +0900

    fix_写真詳細コンポーネント修正

--- web/resources/js/pages/PhotoDetail.vue
+++ web/resources/js/pages/PhotoDetail.vue
@@ -1,3 +1,54 @@
 <template>
-  <h1>Photo Detail</h1>
+  <div v-if="photo" class="photo-detail">
+    <figure class="photo-detail__pane photo-detail__image">
+      <img :src="'/img/photo_images/'+photo.filename" alt="">
+      <figcaption>Posted by {{ photo.user.name }}</figcaption>
+    </figure>
+    <div class="photo-detail__pane">
+      <button class="button button--like" title="Like photo">
+        <i class="icon ion-md-heart"></i>12
+      </button>
+      <h2 class="photo-detail__title">
+        <i class="icon ion-md-chatboxes"></i>Comments
+      </h2>
+    </div>
+  </div>
 </template>
+
+<script>
+import { OK } from '../util'
+
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      photo: null
+    }
+  },
+  methods: {
+    async fetchPhoto () {
+      const response = await axios.get(`/api/photos/${this.id}`)
+
+      if (response.status !== OK) {
+        this.$store.commit('error/setCode', response.status)
+        return false
+      }
+
+      this.photo = response.data
+    }
+  },
+  watch: {
+    $route: {
+      async handler () {
+        await this.fetchPhoto()
+      },
+      immediate: true
+    }
+  }
+}
+</script>
