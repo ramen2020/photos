7aa6857ecb756b63dd7bf522cb75fd80fc0f8814
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-07-01 13:46:54 +0900

    fix_user、photoテーブルの修正

--- web/database/migrations/2020_06_21_141343_create_photos_table.php
+++ web/database/migrations/2020_06_21_141343_create_photos_table.php
@@ -14,8 +14,8 @@ class CreatePhotosTable extends Migration
     public function up()
     {
         Schema::create('photos', function (Blueprint $table) {
-            $table->string('id');
-            $table->bigIncrements('user_id');
+            $table->string('id')->primary();
+            $table->integer('user_id')->unsigned()->index();
             $table->string('filename');
             $table->timestamps();
 
